d3a7f259cd400f0682b90fcb26e2d0e5c6adc388
lucacris72 <lucacris72@gmail.com>
2025-06-10 20:58:16 +0000

    fixed port number

--- ebpf-labs/project_24-25/tests/test_server.sh
+++ ebpf-labs/project_24-25/tests/test_server.sh
@@ -1,5 +1,5 @@
 export TARGET_IP_ADDR="10.0.0.1"
-export TEST_PORT=8080
+export TEST_PORT=54321
 
 echo "Start listener socat on $TARGET_IP_ADDR:$TEST_PORT..."
 socat TCP4-LISTEN:$TEST_PORT,bind=$TARGET_IP_ADDR,fork,reuseaddr -
